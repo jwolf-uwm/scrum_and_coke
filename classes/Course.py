8fd7c8d2f267d5c12cc03f7b89f18c9ed0c89251
EvanVetrano <evanv1998@gmail.com>
2019-03-08 17:05:04 -0600

    updates in Supervisor class, test_supervisor, and creates Courses class

---
+++ classes/Course.py
@@ -0,0 +1,7 @@
+
+class Course:
+    def __init__(self, course_name, course_title):
+        self.name = course_name
+        self.title - course_title
+        self.courseSection = []
+        self.labSection = []
