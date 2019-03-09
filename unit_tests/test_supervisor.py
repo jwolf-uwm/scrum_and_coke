60d3be85872e7e63b0af1408ca137a251dc205fa
EvanVetrano <evanv1998@gmail.com>
2019-03-08 22:36:22 -0600

    messing up unit tests. what else is new

--- unit_tests/test_supervisor.py
+++ unit_tests/test_supervisor.py
@@ -2,34 +2,23 @@
 
 from unittest import TestCase
 from classes.Supervisor import Supervisor
-from classes.Instructor import Instructor
-from classes.Course import Course
-from classes.TA import TA
 from classes.Administrator import Administrator
+from classes.Instructor import Instructor
+from classes.TA import TA
+from classes.Course import Course
 
 
 class TestSupervisor(TestCase):
-
-    def test_init(self):
-        sup1 = Supervisor("sup1@uwm.edu", "sup1pass")
-        self.assertEqual(sup1.email, "sup1@uwm.edu")
-        self.assertEqual(sup1.password, "sup1pass")
-        self.assertEqual(sup1.name, "DEFAULT")
-        self.assertEqual(sup1.phone_number, "DEFAULT")
+    sup = Supervisor("sup@uwm.edu", "sup_pass")
+    admin = Administrator("admin@uwm.edu", "admin_pass")
+    ins = Instructor("ins@uwm.edu", "ins_pass")
+    ta = TA("ta@uwm.edu", "ta_pass")
+    course1 = Course("Intro to Computer Stuff", "CS101")
 
     def test_assign_instructor_course(self):
-        sup1 = Supervisor("sup1@uwm.edu", "sup1pass")
-        self.Ins1 = Instructor("ins1@uwm.edu", "ins1pass")
-        self.Course1 = Course("CS101", 401)
-
-        # best case
-        sup1.assign_instuctor("ins1@uwm.edu", "CS101", 401)
-        self.assertEqual(Ins1.course)
+        
 
     def test_assign_ta_course(self):
-        ta1 = TA("TA1@uwm.edu", "TA1")
-        course1 = Course("Intro to Computer Stuff", "CS101")
-
 
 
     def test_assign_ta_lab(self):
