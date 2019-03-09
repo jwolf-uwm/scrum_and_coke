ebe5d8d80acee62111076ee91e3a701891aaf83c
Jeffrey Wolf <lwolf@uwm.edu>
2019-03-08 23:25:30 -0600

    test_instructor success cases

--- unit_tests/test_instructor.py
+++ unit_tests/test_instructor.py
@@ -8,8 +8,12 @@ from unittest import TestCase
 
 class TestInstructor(TestCase):
 
-    def test___init__(self):
+    def setup(self):
         self.instructor1 = Instructor("DEFAULT_EMAIL", "DEFAULT_PASSWORD")
+        self.ta1 = TA("DEFAULT_TA1_EMAIL", "DEFAULT_PASSWORD")
+        self.course1 = Course("DEFAULT_ID", 101)
+
+    def test___init__(self):
         self.assertEquals(self.instructor1.email, "DEFAULT_EMAIL")
         self.assertEquals(self.instructor1.password, "DEFAULT_PASSWORD")
         self.assertEquals(self.instructor1.name, "DEFAULT")
@@ -35,4 +39,23 @@ class TestInstructor(TestCase):
         self.instructor1.edit_contact_info("email", "bob_ross@not_real.arf")
         self.assertEquals(self.instructor1.email, "bob_ross@not_real.arf")
 
+        with self.assertRaises(TypeError):
+            self.instructor1.edit_contact_info(2, "Ted")
 
+        with self.assertRaises(TypeError):
+            self.instructor1.edit_contact_info("name", 41.6)
+
+    def test_read_public_contact(self):
+        self.assertEquals(self.instructor1.read_public_contact(), "Bob Ross, bob_ross@not_real.arf")
+
+    def test_send_notification_ta(self):
+        self.assertTrue(self.instructor1.send_notification_ta("DEFAULT_TA_EMAIL", "Hi!"))
+        self.assertFalse(self.instructor1.send_notification_ta("ROAR", "Woof!"))
+
+    def test_view_course(self):
+        self.assertEquals(self.instructor1.view_course_assign(), "No courses assigned.")
+        self.instructor1.courses.append(self.course1)
+        self.assertEquals(self.instructor1.view_course_assign(), "DEFAULT_ID - 101")
+
+    def test_view_ta_assign(self):
+        self.assertEquals(self.instructor1.view_ta_assign()[0], self.ta1)
