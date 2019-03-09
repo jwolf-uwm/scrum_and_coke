ee4157525b63dfeec85b2a82a9aae18fb884e59b
EvanVetrano <evanv1998@gmail.com>
2019-03-08 22:00:41 -0600

    edited admin with new test cases

--- unit_tests/test_admin.py
+++ unit_tests/test_admin.py
@@ -4,17 +4,16 @@ from classes.Administrator import Administrator
 from unittest import TestCase
 
 
-class TestAdmin(TestCase):
-
-    def test_createCourse(self):
+class TestAdministrator(TestCase):
+    def test_create_course(self):
         self.ad1 = Administrator("ad1@uwm.edu", "ad1pas")
         self.ad2 = Administrator("ad22@uwm.edu", "ad2pass")
 
-        self.assertTrue(self, self.ad1.createAccount("people","pass","thatguy@uwm.edu"))
+        self.assertTrue(self, self.ad1.createAccount("people", "pass", "thatguy@uwm.edu"))
         self.assertTrue(self, self.ad1.createAccount("people1", "pass2", "thatguy2@uwm.edu"))
-        self.assertFalse(self, self.ad1.createAccount("people","pass","thatguy@uwm.edu"))
+        self.assertFalse(self, self.ad1.createAccount("people", "pass", "thatguy@uwm.edu"))
 
-    def test_createAccount(self):
+    def test_create_account(self):
         self.ad1 = Administrator("admin", "testpass", "blah@uwm.edu")
         self.ad2 = Administrator("admin2", "testpass2", "blah2@uwm.edu")
 
@@ -22,6 +21,15 @@ class TestAdmin(TestCase):
         self.assertTrue(self, self.ad2.createCourse("CS315"))
         self.assertFalse(self, self.ad1.createCourse("CS337"))
 
-    def test_editAccount(self):
+    def test_edit_account(self):
         self.ad1 = Administrator("admin", "testpass", "blah@uwm.edu")
         self.ad2 = Administrator("admin2", "testpass2", "blah2@uwm.edu")
+
+    def test_delete_account(self):
+        self.fail()
+
+    def test_send_notification(self):
+        self.fail()
+
+    def test_access_info(self):
+        self.fail()
