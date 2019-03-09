61acbd473ff63f075cc3e96ee583eb021be54e46
EvanVetrano <evanv1998@gmail.com>
2019-03-09 10:39:27 -0600

    messing up unit tests. what else is new

--- unit_tests/test_supervisor.py
+++ unit_tests/test_supervisor.py
@@ -2,18 +2,10 @@
 
 from unittest import TestCase
 from classes.Supervisor import Supervisor
-from classes.Administrator import Administrator
-from classes.Instructor import Instructor
-from classes.TA import TA
-from classes.Course import Course
 
 
 class TestSupervisor(TestCase):
     sup = Supervisor("sup@uwm.edu", "sup_pass")
-    admin = Administrator("admin@uwm.edu", "admin_pass")
-    ins = Instructor("ins@uwm.edu", "ins_pass")
-    ta = TA("ta@uwm.edu", "ta_pass")
-    course1 = Course("Intro to Computer Stuff", "CS101")
 
     def test_assign_instructor_course(self):
 
