b9882b15f955465402590895e16e36e70a4dc55b
EvanVetrano <evanv1998@gmail.com>
2019-03-08 20:58:05 -0600

    edits to fit new person model

--- classes/TA.py
+++ classes/TA.py
@@ -5,11 +5,15 @@ from classes import Person
 
 class TA(Person):
 
-    def __init__(self, email, username, password):
-        super().__init__(email, username, password)
-        self.name = "DEFAULT"
-        self.phone = "DEFAULT"
+    def __init__(self, email, password):
+        super().__init__(email, password)
         self.courses = []
 
-    def __view_TA_assignments__(self, assignments):
-        return assignments
+    def view_ta_assignments(self):
+        return
+
+    def read_public_contact_info(self):
+        return
+
+    def view_ta_assignments(self):
+        return
