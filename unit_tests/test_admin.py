013b81221b0e50ff895a203b9ec596a34875a750
EvanVetrano <evanv1998@gmail.com>
2019-03-09 13:06:48 -0600

    test_admin changes

--- unit_tests/test_admin.py
+++ unit_tests/test_admin.py
@@ -5,18 +5,16 @@ from unittest import TestCase
 
 
 class TestAdministrator(TestCase):
-    def test_create_course(self):
+    def setUp(self):
         self.ad1 = Administrator("ad1@uwm.edu", "ad1pas")
         self.ad2 = Administrator("ad22@uwm.edu", "ad2pass")
 
+    def test_create_course(self):
         self.assertTrue(self, self.ad1.createAccount("people", "pass", "thatguy@uwm.edu"))
         self.assertTrue(self, self.ad1.createAccount("people1", "pass2", "thatguy2@uwm.edu"))
         self.assertFalse(self, self.ad1.createAccount("people", "pass", "thatguy@uwm.edu"))
 
     def test_create_account(self):
-        self.ad1 = Administrator("admin", "testpass", "blah@uwm.edu")
-        self.ad2 = Administrator("admin2", "testpass2", "blah2@uwm.edu")
-
         self.assertTrue(self, self.ad1.createCourse("CS337"))
         self.assertTrue(self, self.ad2.createCourse("CS315"))
         self.assertFalse(self, self.ad1.createCourse("CS337"))
