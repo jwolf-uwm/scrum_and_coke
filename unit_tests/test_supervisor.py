9440c39c3952b25f2a4a6562c2b0be14484bbc0e
EvanVetrano <evanv1998@gmail.com>
2019-03-09 12:54:45 -0600

    test_supervisor changes

--- unit_tests/test_supervisor.py
+++ unit_tests/test_supervisor.py
@@ -13,22 +13,22 @@ class TestSupervisor(TestCase):
         self.ins1 = "ins1@uwm.edu"
         self.ins2_courses = []
         self.ins2 = "in2s@uwm.edu"
-
         # fake course
         self.course1_tas = []
-        self.course1_instructor
+        self.course1_instructor = ""
         self.course1 = ("CS101", 2)
         self.course2_tas = []
-        self.course2_instructor
+        self.course2_instructor = ""
         self.course2 = ("CS202", 0)
-
         # fake ta
         self.ta1_sections = []
-        self.ta1_course
+        self.ta1_course = ""
         self.ta1 = "ta1@uwm.edu"
         self.ta2_sections = []
-        self.ta2_course
+        self.ta2_course = ""
         self.ta2 = "ta2@uwm.edu"
+        self.ta3 = "ta3.uwm.edu"
+        self.ta3_course = ""
 
     def test_assign_instructor_course(self):
         # instructor 1 is assigned CS101
@@ -60,13 +60,36 @@ class TestSupervisor(TestCase):
         self.assertEqual(self.ta1_course, "CS202")
         self.assertEqual(self.course2_tas[0], "ta1@uwm.edu")
 
-        # instructor 2 is assigned CS101
-        self.sup.assign_ta_course(self.ins2, self.course1[0])
-        self.assertEqual(self.ins2_courses[0], "CS101")
-        self.assertEqual(self.course1_instructor, "ins2@uwm.edu")
-        self.assertNotEqual(self.ins1_courses[0], "CS101")
+        # TA 2 is assigned CS101
+        self.sup.assign_ta_course(self.ta2, self.course1[0])
+        self.assertEqual(self.ta2_course, "CS101")
+        self.assertEqual(self.course1_tas[1], "ins2@uwm.edu")
+        self.assertEqual(self.course1_tas[0], "ins1@uwm.edu")
 
-        self.assertRaises(self.sup.assign_instructor(self.ta1, self.course1[0]), TypeError)
+        # Try to assign a third TA to CS101
+        self.sup.assign_ta_course(self.ta3, self.course1[0])
+        self.assertNotEqual(self.ta3_course, "CS101")
+        self.assertNotEqual(self.course1_tas[2], "ins3@uwm.edu")
+
+        self.assertRaises(self.sup.assign_instructor(self.ins1, self.course1[0]), TypeError)
 
     def test_assign_ta_lab(self):
+        # TA 1 is assigned CS101 - 801
+        self.sup.assign_ta_lab(self.ta1, 801)
+        self.assertEqual(self.ta1_sections[0], 801)
 
+        # TA 2 is assigned CS101
+        self.sup.assign_ta_course(self.ta2, self.course1[0])
+        self.assertEqual(self.ta2_sections[0], "CS101")
+
+        # assign TA 1 another lab section
+        self.sup.assign_ta_course(self.ta1, self.course2[0])
+        self.assertEqual(self.ta1_course, "CS202")
+        self.assertEqual(self.course2_tas[0], "ta1@uwm.edu")
+
+        # Try to assign a third TA to CS101
+        self.sup.assign_ta_course(self.ta3, self.course1[0])
+        self.assertNotEqual(self.ta3_course, "CS101")
+        self.assertNotEqual(self.course1_tas[2], "ins3@uwm.edu")
+
+        self.assertRaises(self.sup.assign_instructor(self.ins1, self.course1[0]), TypeError)
