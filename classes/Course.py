186e304d2e4272b8e8bb059ee54f09f952d63922
Jeffrey Wolf <lwolf@uwm.edu>
2019-03-08 20:54:07 -0600

    Course stubbed

--- classes/Course.py
+++ classes/Course.py
@@ -1,7 +1,39 @@
 
+
 class Course:
-    def __init__(self, course_name, course_title):
-        self.name = course_name
-        self.title - course_title
-        self.courseSection = []
-        self.labSection = []
+    def __init__(self, course_id, section_number):
+        self.course_id = course_id
+        self.section_number = section_number
+        self.lab_sections = []
+        self.instructor
+        self.tee_ays = []
+
+    def set_course_id(self, course_id):
+        return
+
+    def set_course_section(self, section_number):
+        return
+
+    def set_instructor(self, instructor):
+        return
+
+    def add_lab_section(self, lab_section):
+        return
+
+    def add_tee_ay(self, tee_ay):
+        return
+
+    def get_course_id(self):
+        return
+
+    def get_course_name(self):
+        return
+
+    def get_course_section(self):
+        return
+
+    def get_lab_sections(self):
+        return
+
+    def get_tee_ays(self):
+        return
