01386dc8f568e0907221c2937d15fe76d8f145ed
EvanVetrano <evanv1998@gmail.com>
2019-03-09 10:45:22 -0600

    setup sup

--- unit_tests/test_supervisor.py
+++ unit_tests/test_supervisor.py
@@ -5,7 +5,12 @@ from classes.Supervisor import Supervisor
 
 
 class TestSupervisor(TestCase):
-    sup = Supervisor("sup@uwm.edu", "sup_pass")
+    def setUp(self):
+        self.sup = Supervisor("sup@uwm.edu", "sup_pass")
+        # fake fields
+        self.adm = ("adm@uwm.edu", "amd_pass")
+        self.ins = ("ins@uwm.edu", "ins_pass")
+        self.tas = ("tas@uwm.edu", "tas_pass")
 
     def test_assign_instructor_course(self):
 
