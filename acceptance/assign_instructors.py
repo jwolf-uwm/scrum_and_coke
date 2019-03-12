193db3a5d078183660f59f43555b506169234408
Jeffrey Wolf <lwolf@uwm.edu>
2019-03-12 17:22:41 -0500

    assign_isntructors done

--- acceptance/assign_instructors.py
+++ acceptance/assign_instructors.py
@@ -1,22 +1,21 @@
-import unittest
+from unittest import TestCase
+from classes.Supervisor import Supervisor
+from classes.Administrator import Administrator
+from classes.Instructor import Instructor
+from classes.TA import TA
+from classes.Course import Course
 
 
-class AssignInstructorTests(unittest.TestCase):
+class AssignInstructorTests(TestCase):
     def setup(self):
-
-        # Think we should instantiate things once we have unit tests, and not use commands for this. - Jeff
-
-        self.ui.command("create_user Supervisor SupervisorPassword")
-        self.ui.command("create_user Administrator AdministratorPassword")
-        self.ui.command("create_user Instructor InstructorId InstructorPassword")
-        self.ui.command("create_user Instructor2 InstructorId2 InstructorPassword2")
-        self.ui.command("create_user TA TAId TAPassword")
-        self.ui.command("create_cs_class SomeCSClass CS101")
-        self.ui.command("create_cs_class SomeCSClass2 CS102")
-        self.ui.command("create_cs_class SomeCSClass3 CS103")
-        self.ui.command("create_cs_class SomeCSClass4 CS104")
-        self.ui.command("create_cs_class SomeCSClass5 CS105")
-        self.ui.command("create_cs_class SomeCSClass6 CS106")
+        self.Sup = Supervisor("supervisor@uwm.edu", "SupervisorPassword")
+        self.Admin = Administrator("admin@uwm.edu", "AdministratorPassword")
+        self.Inst = Instructor("instructor@uwm.edu", "InstructorPassword")
+        self.Inst2 = Instructor("instructor2@uwm.edu", "InstructorPassword2")
+        self.TA = TA("ta@uwm.edu", "TAPassword")
+        self.Course1 = Course("SomeCSClass1", 1)
+        self.Course2 = Course("SomeCSClass2", 2)
+        self.Course3 = Course("SomeCSClass3", 3)
 
     """
         Assigning an instructor requires user logged in as Supervisor or Administrator.
@@ -34,74 +33,57 @@ class AssignInstructorTests(unittest.TestCase):
         - "Only instructors can be assigned to classes."
         If the user is not logged in as a Supervisor or Administrator, failure:
         - "You are not authorized to assign instructors."
-    """
-
-    """ 
-        NOTE: Should we add the functionality to remove/edit a course from an instructor, or create
-        a new user story for this? - Jeff
+        If an invalid email address is used for the instructor, failure:
+        - "Invalid email address."
+        If the course entered does not exist, failure:
+        - "Course does not exist."
+        If the instructor entered does not exist, failure:
+        - "Instructor does not exist."
     """
 
     def test_command_assign_instructor_supervisor(self):
-        self.ui.command("login Supervisor SupervisorPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor.id SomeCSClass"),
-                         "Instructor " + Instructor.name + " assigned to " + SomeCSClass.name + ".")
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor@uwm.edu SomeCSClass1"),
+                         "Instructor " + self.Inst.name + " assigned to " + self.Course1.course_id + ".")
 
     def test_command_assign_instructor_administrator(self):
-        self.ui.command("login Administrator AdministratorPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor.id SomeCSClass2"),
-                         "Instructor " + Instructor.name + " assigned to " + SomeCSClass2.name + ".")
+        self.ui.command("login admin@uwm.edu AdministratorPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor@uwm.edu SomeCSClass2"),
+                         "Instructor " + self.Inst.name + " assigned to " + self.Course1.course_id + ".")
 
     def test_command_assign_instructor_instructor(self):
-        self.ui.command("login Instructor InstructorPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor.id SomeCSClass3"),
+        self.ui.command("login instructor@uwm.edu InstructorPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor@uwm.edu SomeCSClass3"),
                          "You are not authorized to assign instructors.")
 
     def test_command_assign_instructor_TA(self):
-        self.ui.command("login TA TAPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor.id SomeCSClass3"),
+        self.ui.command("login ta@uwm.edu TAPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor@uwm.edu SomeCSClass3"),
                          "You are not authorized to assign instructors.")
 
     def test_command_assign_instructor_class_taken(self):
-        self.ui.command("login Supervisor SupervisorPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor2.id SomeCSClass"),
-                         SomeCSClass.name + " already has an instructor.")
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor2@uwm.edu SomeCSClass1"),
+                         self.Course1.course_id + " already has an instructor.")
 
     def test_command_assign_instructor_invalid_arguments(self):
-        self.ui.command("login Supervisor SupervisorPassword")
-        self.assertEqual(self.ui.command("assign_instructor Instructor.id"),
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor instructor@uwm.edu"),
                          "Invalid arguments in command.")
 
     def test_command_assign_instructor_assign_TA(self):
-        self.ui.command("login Supervisor SupervisorPassword")
-        self.assertEqual(self.ui.command("assign_instructor TA.id SomeCSClass3"),
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor ta@uwm.edu SomeCSClass3"),
                          "Only instructors can be assigned to classes.")
 
-    def more_tests_to_sort_through_later(self):
-        self.assertEqual(self.ui.command("assign_instructor ins1@uwm.edu CS101"), "ins1@uwm.edu was assigned to CS101")
+    def test_command_invalid_email(self):
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor ins1, SomeCSClass3"), "Invalid email address.")
 
-        # entered arguments that don't exist
-        self.assertEqual(self.ui.command("assign_instructor ins1, 801"), "Error: invalid email address")
-        self.assertEqual(self.ui.command("assign_instructor ins1@uwm.edu CS102"), "CS102 could not be assigned since"
-                                                                                  " it does not exist")
-        self.assertEqual(self.ui.command("assign_instructor ins2@uwm.edu CS101"), "ins2@uwm.edu could not be assigned "
-                                                                                  "since it does not exist")
+    def test_command_nonexistent_course(self):
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor ins1@uwm.edu SomeCSClass4"), "Course does not exist.")
 
-        # test number of arguments
-        self.assertEqual(self.ui.command("assign_instructor ins1@uwm.edu"), "Error: too few arguments")
-        self.assertEqual(self.ui.command("assign_instructor"), "Error: too few arguments")
-
-        # test if class already has instructor
-        self.Ins2 = Instructor("ins2@uwm.edu", "ins2pass")
-        self.assertEqual(self.ui.command("assign_instructor ins2@uwm.edu CS101"), "CS101 already has been assigned an"
-                                                                                  " instructor")
-
-        # test against non-instructors
-        self.TA = TA("TA@uwm.edu", "TA")
-        self.SUP = Supervisor("SUP@uwm.edu", "SUP")
-        self.ADMIN = Administrator("ADMN@uwm.edu", "ADMIN")
-        self.assertEqual(self.ui.command("assign_instructor TA@uwm.edu CS101"), "Error: TA@uwm.edu is not an "
-                                                                                "instructor")
-        self.assertEqual(self.ui.command("assign_instructor SUP@uwm.edu CS101"), "Error: SUP@uwm.edu is not an "
-                                                                                 "instructor")
-        self.assertEqual(self.ui.command("assign_instructor ADMIN@uwm.edu CS101"), "Error: ADMIN@uwm.edu is not an "
-                                                                                   "instructor")
+    def test_command_nonexistent_instructor(self):
+        self.ui.command("login supervisor@uwm.edu SupervisorPassword")
+        self.assertEqual(self.ui.command("assign_instructor ins4@uwm.edu SomeCSClass3"), "Instructor does not exist.")
